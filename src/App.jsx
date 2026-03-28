4e4d0e62dbc86ba26ab5be3a91657e1610365ed8
paridhi-rawat <paridhir279@gmail.com>
2026-03-28 17:41:13 +0530

    Hello world

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+       
+          <h1></h1>Hello world
+        
+      </section>
+
+    </>
+  )
+}
+
+export default App
