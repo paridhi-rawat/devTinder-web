b6944d773cebfa7a90d6eb0b7062769fe760af57
paridhi-rawat <paridhir279@gmail.com>
2026-03-29 13:08:59 +0530

    not access other route without login, if token is not present redirect to login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,31 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Footer from "./Footer"
 import NavBar from "./NavBar"
-
+import { useEffect } from "react"
+import axios from "axios"
+import {BASE_URL} from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "../utils/userSlice"
 const Body = () => {
+  const dispatch = useDispatch()
+  const userData = useSelector((store) => store.user)
+  const navigate = useNavigate()
+
+  const fetchUser = async () =>{
+    if(userData) return;
+    try{
+      const res = await axios.get(BASE_URL + "/profile/view",  { withCredentials: true })
+      dispatch(addUser(res.data))
+    }catch(err){
+      if(err.status == 401){
+        navigate("/login")
+      }
+      console.log("err",err)
+    }
+  }
+  useEffect(()=>{
+    fetchUser()
+  },[])
   return (
    <div>
     <NavBar/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,30 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 const NavBar = () => {
   const user = useSelector((store) => store.user)
-  console.log("user",user)
   return (
      <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Dev Tinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">Dev Tinder</Link>
         </div>
         <div className="flex gap-2">
           {user && <div className="dropdown dropdown-end mr-5">
-            <span className="mr-5"> welcome, {user.firstName}</span>
+            <span className="mr-5">welcome, {user.firstName}</span>
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src={user.photo} />
+                  src="https://lh3.googleusercontent.com/a/ACg8ocKS9dHOpXpOdjSP8btiYiasBYKWff7GiaoVINOAX_p9yvzG0nE0=s96-c" />
               </div>
             </div>
             <ul
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
